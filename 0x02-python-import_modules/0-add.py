7471ab34ba884b575812826bc17573245d9e3df8
Milli-saved <million12.tenkir@gmail.com>
2021-11-21 22:28:56 -0800

    first q fix

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,8 @@
-from add_0 import add as addition
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from add_0 import add as addition
 
-a = 1
-b = 2
-c = addition(a, b)
-print("{:d} + {:d} = {:d}".format(a, b, c))
+    a = 1
+    b = 2
+    c = addition(a, b)
+    print("{:d} + {:d} = {:d}".format(a, b, c))
